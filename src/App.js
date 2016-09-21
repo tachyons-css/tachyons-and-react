2d7b2f85ea3564d1b5c4b4827678b9ad00d4de0c
John Otander <johnotander@gmail.com>
2016-09-21 12:54:48 -0600

    Add a header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react'
+import Header from './components/header'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        Hello, world!
-      </div>
-    )
+    return <Header />
   }
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from '../logo.jpg'
+
+export default () => (
+  <header className='tc pv4 pv5-ns'>
+    <img src={logo} className='br-100 pa1 ba b--black-10 h3 w3' alt='avatar' />
+    <h1 className='f5 f4-ns fw6 mid-gray'>Jasper Whitehouse</h1>
+    <h2 className='f6 gray fw2 ttu tracked'>Los Angeles</h2>
+  </header>
+)
