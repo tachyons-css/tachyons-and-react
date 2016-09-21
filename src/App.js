22deea9bc5cf9fcb8fed7bd68629c3d67e9b17f7
John Otander <johnotander@gmail.com>
2016-09-21 12:34:20 -0600

    Complete tachyons integration

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        Hello, world!
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
